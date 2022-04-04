0ebe3a9f5939ed17faf6cdc451530f02e0fc2a56
Ivar Bredeli <ivabr@stud.ntnu.no>
2022-04-04 16:07:14 +0200

    49% Done, preAuthorize.

--- src/main/java/no/ntnu/springsecurtiy/student/StudentManagementController.java
+++ src/main/java/no/ntnu/springsecurtiy/student/StudentManagementController.java
@@ -1,6 +1,7 @@
 package no.ntnu.springsecurtiy.student;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -18,22 +19,26 @@ public class StudentManagementController {
 
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         System.out.println("getAllStudents");
         return students;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("registerNewStudent: " + student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println("deleteStudent: " + studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.printf("updateStudent %s %s%n", studentId, student);
     }
